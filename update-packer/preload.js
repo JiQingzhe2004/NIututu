642ce7885415052f125536a1f8e04208f89351bb
吉庆喆 <304028273@qq.com>
2025-05-18 21:40:24 +0800

    去除更新工具

--- update-packer/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-	getTags: () => ipcRenderer.invoke('get-tags'),
-	runPack: (oldTag, newTag) => ipcRenderer.invoke('run-pack', oldTag, newTag),
-	onProgress: (callback) => ipcRenderer.on('progress', (_, msg) => callback(msg))
-});

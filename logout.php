f274573de93230923a1dd676fdb730f5009f47d4
吉庆喆 <304028273@qq.com>
2025-05-18 18:54:34 +0800

    feat: 注销时清除 remember_token 并移除相关 Cookie，更新字段及新增 SVG

--- logout.php
+++ logout.php
@@ -1,5 +1,16 @@
 <?php
 session_start();
+
+// 清除数据库中的 remember_token
+if (isset($_SESSION['user'])) {
+    require 'config.php';
+    $stmt = $pdo->prepare('UPDATE users SET remember_token = NULL WHERE id = ?');
+    $stmt->execute([$_SESSION['user']['id']]);
+}
+
+// 清除浏览器 remember_token cookie
+setcookie('remember_token', '', time() - 3600, '/', '', isset($_SERVER['HTTPS']), true);
+
 session_destroy();
 header('Location: login.php');
 exit();
